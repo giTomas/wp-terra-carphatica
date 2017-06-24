d50a42ed06215066d4a6addcabb012c3cda28190
Tomas Kosegi <tomas.kosegi@gmail.com>
2017-06-24 13:34:39 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Terra Carphatica</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
