686df3417a250b125472d939dce8f7ab6ea55e8b
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2017-07-14 10:16:36 +0200

    refactoring htttp module

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,7 @@ export const POSTS_LIST_RESET = 'POST_LIST_RESET'
 // export const SET_POST_ID = 'SET_POST_ID';
 
 
-// loading
+// GENERIC DATA EVENTS
 export const _SUCCESS      			= '_SUCCESS';
 export const _LOADING     		  = '_LOADING';
 export const _ERROR							= '_ERROR';
@@ -19,18 +19,25 @@ export const _RESET             = '_RESET';
 
 // new concept
 export const MEMBERS            = 'MEMBERS';
+export const MEMBERS						= 'MEMBER';
+export const CATEGORIES         = 'CATEGORIES';
+export const CATEGORY           = 'CATEGORY';
+export const POST 							= 'POST';
+
 export const MEMBERS_SUCCESS    = 'MEMBERS_SUCCESS';
 export const MEMBERS_LOADING    = 'MEMBERS_LOADING';
 export const MEMBERS_ERROR      = 'MEMBERS_ERROR';
 // export const MEMBERS_RESET   = 'POST_LIST_RESET';
 
-export const CATEGORIES         = 'CATEGORIES';
+
+
 export const CATEGORIES_SUCCESS = 'CATEGORIES_SUCCESS';
 export const CATEGORIES_LOADING = 'CATEGORIES_LOADING';
 export const CATEGORIES_ERROR   = 'CATEGORIES_ERROR';
 // export const CATEGORIES_RESET   = 'POST_LIST_RESET';
 
-export const POSTS              = 'POSTS_LIST';
+
+
 export const POSTS_LIST_SUCCESS = 'POSTS_LIST_SUCCESS';
 export const POSTS_LIST_LOADING = 'POSTS_LIST_LOADING';
 export const POST_LIST_ERROR    = 'POST_LIST_ERROR';
--- src/http/index.js
+++ src/http/index.js
@@ -1,54 +1,13 @@
-import R from 'ramda';
+import { partial } from 'ramda';
+import { fetchJson } from './fetch';
+import {
+	URL_MEMBERS,
+	URL_SECTIONS,
+	URL_CATEGORIES,
+	URL_MEMBERS_PER_PAGE,
+	URL_CATEGORY,
+} from './urls';
 
-const URL = 'http://localhost/wp-json/wp/v2'
-const SECTIONS_URL = `${URL}`;
-const MEMBERS_URL = `${URL}/users?fields=id,slug,acf,description,name,display`;
-// const MEMBER_URL = `${URL}/members/`
-// const MEMBERS_URL = `${URL}users/`
-// const PRIRODA_ALL = `${URL}/posts?category=`;
-const MEMBERS_PER_PAGE = `${URL}/members?_embed&per_page=2&page=`
-const URL_CATEGORIES = `${URL}/categories?fields=id,slug,name`;
-const URL_CATEGORY = `${URL}/posts?fields=id,slug,author,date,title&categories=`;
-// const URL_POST = `${URL}/`;
-
-async function fetchJson(url, id='') {
-  const response = await fetch(url+id);
-  const data = await response.json();
-
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-
-  return data;
-}
-
-// export async function fetchMember(id) {
-//   const url = `${MEMBER_URL}${id}`;
-//   return fetchJson(url);
-// }
-
-// async function fetchPost(url, id='') {
-//   const response = await fetch(url+id);
-//   const data = await response.json();
-//   const headers = await response.headers;
-//   if (!response.ok) {
-//     throw Error(response.statusText);
-//   }
-//   return {
-//     data,
-//     total: headers.get('X-WP-Total'),
-//     pages: headers.get('X-WP-TotalPages'),
-//   }
-// }
-// 
-// async function fetchPost(url, id='') {
-//   const response = await fetch(url+id);
-//   const data = await response.json();
-//   if (!response.ok) {
-//     throw Error(response.statusText);
-//   }
-//   return data;
-// }
 
 export const fetchMembers     = R.partial(fetchJson, [MEMBERS_URL]);
 export const fetchSections    = R.partial(fetchJson, [SECTIONS_URL]);
